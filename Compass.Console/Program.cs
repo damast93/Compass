ab6e60f4efdd7245ddb510d37dffa2a3f1aa0e0f
Henrik Ilgen <henrik.ilgen@netzkern.de>
2021-04-23 22:59:15 +0200

    Implement command-line parameters wie System.CommandLine

--- Compass.Console/Program.cs
+++ Compass.Console/Program.cs
@@ -1,20 +1,56 @@
-﻿namespace Compass.Console
-{
-    using System.IO;
-    using System.Threading.Tasks;
-    using Compass.Core.Interpreter;
-    using Parser;
+﻿using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Threading.Tasks;
+using Compass.Core.Interpreter;
 
+namespace Compass.Console
+{
     class Program
     {
         static async Task Main(string[] args)
         {
-            var input = await File.ReadAllTextAsync(args[0]);
+            var command = new RootCommand("Renders the given Compass script and outputs the rendering as an image file.") {
+                new Argument<FileInfo>(
+                    "input-file",
+                    "The input file to process."
+                ) {
+                    Arity = ArgumentArity.ExactlyOne,
+                },
+                new Option<FileInfo>(
+                    new [] { "--output-file", "-o"},
+                    () => new FileInfo("out.png"),
+                    "The output file to render to."
+                ),
+                new Option<int>(
+                    new[] {"--width", "-w"},
+                    () => 640,
+                    "The width of the image to render."
+                ),
+                new Option<int>(
+                    new[] {"--height", "-h"},
+                    () => 480,
+                    "The height of the image to render."
+                ),
+            };
 
-            using var renderer = new ImageSharpRenderer(640, 480);
+            command.Handler = CommandHandler.Create<FileInfo, FileInfo, int, int>(RenderScriptAsync);
+
+            await command.InvokeAsync(args);
+        }
+
+        private static async Task RenderScriptAsync(
+            FileInfo inputFile,
+            FileInfo outputFile,
+            int width,
+            int height
+        ) {
+            var input = await File.ReadAllTextAsync(inputFile.FullName);
+
+            using var renderer = new ImageSharpRenderer(width, height);
 
             var scope = new Scope();
-            var expressions = Parser.Parse(input);
+            var expressions = Parser.Parser.Parse(input);
             var context = new Context(renderer);
             var interpreter = new StatementInterpreter(scope, context);
 
@@ -23,8 +59,7 @@
                 statement.Accept(interpreter);
             }
 
-            await renderer.SaveToFileAsync("out.png");
+            await renderer.SaveToFileAsync(outputFile.FullName);
         }
     }
-
 }
